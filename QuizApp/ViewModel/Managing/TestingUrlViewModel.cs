5e273dc469ddb31b16f147f15a4df1f2613f0aa5
NazarLuchyt <luchyt1997@mail.com>
2018-06-05 12:49:19 +0300

    fix

--- QuizApp/ViewModel/Managing/TestingUrlViewModel.cs
+++ QuizApp/ViewModel/Managing/TestingUrlViewModel.cs
@@ -8,12 +8,16 @@ namespace QuizApp.ViewModel.Managing
         public string UrlInstance { set; get; }
         public string Guid { set; get; }
         public string TestGuid { set; get; }
+        [Display(Name="Test name:")]
         public string TestName { set; get; }
         public string Interviewee { set; get; }
+        [Display(Name = "Number of runs:")]
         [MyNumber(ErrorMessage = "Enter corect number")]
         public int NumberOfRuns { set; get; }
+        [Display(Name = "Allowed start date:")]
         [MyDateTime(ErrorMessage = "Please enter corect Date or Time")]
         public string AllowedStartDate { set; get; }
+        [Display(Name = "Allowed end date:")]
         [MyDateTime(ErrorMessage = "Please enter corect Date or Time")]
         public string AllowedEndDate { set; get; }
         public bool IsValid { get; set; }
